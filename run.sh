7d13bddb36ceffef5bd7a923b26d851b1963ed40
crazyivan1289 <crazyivan1289@gmail.com>
2025-01-10 17:10:52 +0300

    Move healthcheck logic to service 2

--- run.sh
+++ run.sh
@@ -5,9 +5,6 @@ set -e
 # Запуск FastAPI
 uvicorn backend.main:app --reload --host 127.0.0.1 --port 8000 2>&1 &
 
-# Запуск Flet
-# flet run --web --port 5030 frontend 2>&1 & 
-
 # Запуск Celery рабочего процесса с 4 воркерами
 celery -A backend.core.tasks worker --loglevel=INFO  -P eventlet 2>&1 &
 
