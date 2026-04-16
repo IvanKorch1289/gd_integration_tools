24781b9140778c7b1ac844373cb87a112aa4f17e
IvanKorch1289 <165717123+IvanKorch1289@users.noreply.github.com>
2026-04-16 10:46:57 +0300

    Add files via upload

---
+++ src/utilities/admin_panel/orderkinds.py
@@ -0,0 +1,25 @@
+from sqladmin import ModelView
+
+from app.infrastructure.db.models.orderkinds import OrderKind
+from app.utilities.admins.base import BaseAdmin
+
+__all__ = ("OrderKindAdmin",)
+
+
+class OrderKindAdmin(ModelView, BaseAdmin, model=OrderKind):
+    """
+    Административная панель для модели OrderKind.
+
+    Атрибуты:
+        column_list (List[str]): Список колонок, отображаемых в таблице.
+        column_searchable_list (List[str]): Список колонок, по которым можно выполнять поиск.
+        column_sortable_list (List[str]): Список колонок, по которым можно сортировать.
+        column_filters (List[str]): Список колонок, по которым можно фильтровать.
+        form_create_rules (List[str]): Список полей, отображаемых в форме создания.
+    """
+
+    column_list = ["id", "name", "skb_uuid", "created_at", "updated_at"]
+    column_searchable_list = ["id", "name", "skb_uuid"]
+    column_sortable_list = ["id", "name", "skb_uuid", "created_at", "updated_at"]
+    column_filters = ["name", "skb_uuid"]
+    form_create_rules = ["name", "skb_uuid"]
---
+++ src/utilities/admin_panel/orders.py
@@ -0,0 +1,46 @@
+from sqladmin import ModelView
+
+from app.infrastructure.db.models.orders import Order
+from app.utilities.admins.base import BaseAdmin
+
+__all__ = ("OrderAdmin",)
+
+
+class OrderAdmin(ModelView, BaseAdmin, model=Order):
+    """
+    Административная панель для модели Order.
+
+    Атрибуты:
+        column_list (List[str]): Список колонок, отображаемых в таблице.
+        column_searchable_list (List[str]): Список колонок, по которым можно выполнять поиск.
+        column_sortable_list (List[str]): Список колонок, по которым можно сортировать.
+        page_size (int): Количество элементов на странице по умолчанию.
+        page_size_options (List[int]): Список доступных вариантов количества элементов на странице.
+        column_filters (List[str]): Список колонок, по которым можно фильтровать.
+        form_create_rules (List[str]): Список полей, отображаемых в форме создания.
+    """
+
+    column_list = [
+        "id",
+        "object_uuid",
+        "pledge_cadastral_number",
+        "pledge_gd_id",
+        "created_at",
+        "updated_at",
+    ]
+    column_searchable_list = [
+        "id",
+        "object_uuid",
+        "pledge_cadastral_number",
+        "pledge_gd_id",
+    ]
+    column_sortable_list = [
+        "id",
+        "object_uuid",
+        "pledge_cadastral_number",
+        "pledge_gd_id",
+        "created_at",
+        "updated_at",
+    ]
+    column_filters = ["object_uuid", "pledge_cadastral_number", "pledge_gd_id"]
+    form_create_rules = ["object_uuid", "pledge_cadastral_number", "pledge_gd_id"]
