786a0df11d07bb72d4c2de75923f016c2b8f39e0
IvanKorch1289 <165717123+IvanKorch1289@users.noreply.github.com>
2026-04-16 11:22:29 +0300

    Add files via upload

---
+++ src/core/enums/database.py
@@ -0,0 +1,46 @@
+from enum import Enum
+
+__all__ = ("DatabaseTypeChoices", "IsolationLevelChoices", "DatabaseProfileChoices")
+
+
+class DatabaseTypeChoices(str, Enum):
+    """
+    Перечисление поддерживаемых реляционных СУБД.
+
+    Значения:
+        postgresql (str): PostgreSQL.
+        oracle (str): Oracle.
+    """
+
+    postgresql = "postgresql"
+    oracle = "oracle"
+
+
+class IsolationLevelChoices(str, Enum):
+    """
+    Перечисление допустимых уровней изоляции транзакций.
+
+    Значения:
+        read_committed (str): Уровень READ COMMITTED.
+        repeatable_read (str): Уровень REPEATABLE READ.
+        serializable (str): Уровень SERIALIZABLE.
+    """
+
+    read_committed = "READ COMMITTED"
+    repeatable_read = "REPEATABLE READ"
+    serializable = "SERIALIZABLE"
+
+
+class DatabaseProfileChoices(str, Enum):
+    """
+    Перечисление профилей подключения к БД.
+
+    Значения:
+        main (str): Основная БД проекта.
+        oracle (str): Внешняя Oracle БД.
+        postgres (str): Внешняя PostgreSQL БД.
+    """
+
+    main = "main"
+    oracle = "oracle"
+    postgres = "postgres"
